bf84c54a9e5d8648d041eb73dd3400a2d6ba936b
SungChanYoung <zkzk7290@naver.com>
2025-05-09 15:44:21 +0900

    5차시 과제

--- java_study/src/main/java/day5/homework/SentimentAnalysis.java
+++ java_study/src/main/java/day5/homework/SentimentAnalysis.java
@@ -1,9 +1,61 @@
 package day5.homework;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
 public class SentimentAnalysis implements TextAnalysisStrategy {
+//감정 분석 클래스  부정 긍정 클래스
+    private String[] positive = {
+        "희망", "성장", "행복", "사랑", "믿음",
+        "감사", "용기", "열정", "미소", "긍정"
+    };
+
+    private String[] negative = {
+            "절망", "불안", "실패", "분노",
+            "두려움", "외로움", "좌절", "슬픔",
+            "미움", "불신"
+    };
 
     @Override
     public void analyzeText(String text) {
+        if (text == null || text.isEmpty()){
+            throw new IllegalArgumentException("text is null or empty");
+        }
 
+        text = text.toLowerCase().replaceAll("[^a-zA-Z0-9\\s]", " ");
+        String[] words = text.split("\\s+");
+
+        HashSet<String> positiveSet = new HashSet<>(Arrays.asList(positive));
+        HashSet<String> negativeSet = new HashSet<>(Arrays.asList(negative));
+
+        HashMap<String, Integer> positiveMap = new HashMap<>();
+        HashMap<String, Integer> negativeMap = new HashMap<>();
+
+        for (String word : words){
+            if (positiveSet.contains(word)){
+                positiveMap.put(word, positiveMap.getOrDefault(word, 0) + 1);
+            } else if (negativeSet.contains(word)) {
+                negativeMap.put(word, negativeMap.getOrDefault(word, 0) + 1);
+            }
+        }
+
+        for (Map.Entry<String, Integer> entry : positiveMap.entrySet()){
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
+
+        for (Map.Entry<String, Integer> entry : negativeMap.entrySet()){
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
+
+        int positiveCount = positiveMap.values().stream().mapToInt(Integer::intValue).sum();
+        int negativeCount = negativeMap.values().stream().mapToInt(Integer::intValue).sum();
+
+        if (positiveCount > negativeCount){
+            System.out.println("긍정글 입니다");
+        }else {
+            System.out.println("부정글 입니다");
+        }
     }
 }
--- java_study/src/main/java/day5/homework/WordFrequencyAnalysis.java
+++ java_study/src/main/java/day5/homework/WordFrequencyAnalysis.java
@@ -1,8 +1,13 @@
 package day5.homework;
 
-public class WordFrequencyAnalysis implements TextAnalysisStrategy {
+import java.util.*;
 
-    private int count = 0;
+public class WordFrequencyAnalysis implements TextAnalysisStrategy {
+    //단어 빈도 체크
+    private String[] keywords ={
+            "용기", "사랑", "나", "고마워", "처음", "배움",
+            "하루", "는", "배려", "카카오톡", "학교", "거짓말"
+    };
 
 
     @Override
@@ -12,6 +17,23 @@ public class WordFrequencyAnalysis implements TextAnalysisStrategy {
             throw new IllegalArgumentException("text is null or empty");
         }
 
+        text = text.toLowerCase().replaceAll("[^a-zA-Z0-9\\s]", " ");
+        String[] words = text.split("\\s+");
+
+        Map<String, Integer> wordMap = new HashMap<>();
+
+
+        for (String word : words){
+            for (String keyword : keywords){
+                if (word.equals(keyword)){
+                    wordMap.put(word, wordMap.getOrDefault(word, 0) + 1);
+                }
+            }
+        }
+
+        for (Map.Entry<String, Integer> entry : wordMap.entrySet()){
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
 
     }
 }
